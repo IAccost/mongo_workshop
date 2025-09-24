c294188e8b334e999abef2f5e300e493fb3ff7df
IKurone <xande.cavalcante.costa@gmail.com>
2025-09-24 20:33:47 -0300

    add CommentDTO class and update Post entity to include comments

--- src/main/java/com/accost/mongoworkshop/domain/Post.java
+++ src/main/java/com/accost/mongoworkshop/domain/Post.java
@@ -1,11 +1,14 @@
 package com.accost.mongoworkshop.domain;
 
 import com.accost.mongoworkshop.dto.AuthorDTO;
+import com.accost.mongoworkshop.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,6 +21,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -79,5 +83,13 @@ public class Post implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 }
