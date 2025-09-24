2d756076ce94602ad52f85bb47fb15a364e8239a
IKurone <xande.cavalcante.costa@gmail.com>
2025-09-23 21:11:34 -0300

    add data initialization for User entities on application startup

---
+++ src/main/java/com/accost/mongoworkshop/config/Instantiation.java
@@ -0,0 +1,23 @@
+package com.accost.mongoworkshop.config;
+
+import com.accost.mongoworkshop.domain.User;
+import com.accost.mongoworkshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository repo;
+    @Override
+    public void run(String... args) throws Exception {
+        repo.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        repo.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
