47b64155536b28ea776ab39063c2448c9f18eb33
IKurone <xande.cavalcante.costa@gmail.com>
2025-09-24 20:00:52 -0300

    add GET endpoint to retrieve a post by its ID and implement corresponding service logic

---
+++ src/main/java/com/accost/mongoworkshop/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.accost.mongoworkshop.resources;
+
+import com.accost.mongoworkshop.domain.Post;
+import com.accost.mongoworkshop.domain.User;
+import com.accost.mongoworkshop.dto.UserDTO;
+import com.accost.mongoworkshop.services.PostService;
+import com.accost.mongoworkshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/accost/mongoworkshop/services/PostService.java
@@ -0,0 +1,24 @@
+package com.accost.mongoworkshop.services;
+
+import com.accost.mongoworkshop.domain.Post;
+import com.accost.mongoworkshop.domain.User;
+import com.accost.mongoworkshop.dto.UserDTO;
+import com.accost.mongoworkshop.repository.PostRepository;
+import com.accost.mongoworkshop.repository.UserRepository;
+import com.accost.mongoworkshop.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found"));
+    }
+
+}
