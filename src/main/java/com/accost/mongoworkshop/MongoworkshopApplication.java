f359426ea13eead979dcebebfa665ad80c54ee32
IKurone <xande.cavalcante.costa@gmail.com>
2025-09-24 14:43:06 -0300

    add UserDTO class and update UserResource to return UserDTO list

--- src/main/java/com/accost/mongoworkshop/MongoworkshopApplication.java
+++ src/main/java/com/accost/mongoworkshop/MongoworkshopApplication.java
@@ -14,14 +14,6 @@ public class MongoworkshopApplication {
 		SpringApplication.run(MongoworkshopApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner initDatabase(UserRepository repository) {
-//		return args -> {
-//			repository.deleteAll();
-//			repository.save(new User(null, "Maria", "maria@gmail.com"));
-//			repository.save(new User(null, "João", "joao@gmail.com"));
-//		};
-//	}
 
 }
 
--- src/main/java/com/accost/mongoworkshop/resources/UserResource.java
+++ src/main/java/com/accost/mongoworkshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.accost.mongoworkshop.resources;
 
 import com.accost.mongoworkshop.domain.User;
+import com.accost.mongoworkshop.dto.UserDTO;
 import com.accost.mongoworkshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
